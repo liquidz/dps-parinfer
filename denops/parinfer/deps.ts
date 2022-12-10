ccaacfafb39ac6c9c260b8479f1b1f6f759be508
liquidz <liquidz.uo@gmail.com>
2022-12-10 18:12:10 +0900

    deps: Update outdated deps

--- denops/parinfer/deps.ts
+++ denops/parinfer/deps.ts
@@ -1,8 +1,8 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.9.3/mod.ts";
-export * as batch from "https://deno.land/x/denops_std@v3.9.3/batch/mod.ts";
-export * as helper from "https://deno.land/x/denops_std@v3.9.3/helper/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.10.0/mod.ts";
+export * as batch from "https://deno.land/x/denops_std@v3.10.0/batch/mod.ts";
+export * as helper from "https://deno.land/x/denops_std@v3.10.0/helper/mod.ts";
 export * as unknownutil from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
-export * as variable from "https://deno.land/x/denops_std@v3.9.3/variable/mod.ts";
+export * as variable from "https://deno.land/x/denops_std@v3.10.0/variable/mod.ts";
 export { default as parinfer } from "npm:parinfer@3.13.1";
 import * as diff from "npm:diff@5.1.0";
 export { diff };
